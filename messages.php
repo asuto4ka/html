fdb7eb76aedc6ab029266adcea8510453c1094e0
root <root@localhost.localdomain>
2016-10-04 19:48:05 +0200

    de l'art

--- messages.php
+++ messages.php
@@ -70,12 +70,15 @@
 		
 		<?php
 			// take user messages
-			   $sql = "SELECT * FROM messages WHERE message_receiver_id = $_SESSION['user_id']";
+		$userId = $_SESSION['userId'];
+		//print_r($_SESSION);
+		$sql = "SELECT * FROM messages WHERE message_receiver_id = $userId";
+		echo $sql;
                global $file_db;
                $result =  $file_db->query($sql);
                $result->setFetchMode(PDO::FETCH_ASSOC);
                $result = $result->fetch();
-		
+		//print_r($result);
 		?>
 
 		<table class="table table-striped">
@@ -87,29 +90,25 @@
 				<td>Supprimer</td>
 				<td>Ouvrir</td>
 			</tr>
-			
 			<?php
-			
-			foreach($result as $row){ ?>
+				foreach($result as $row){ 
+			?>
 			<tr>
-				<td><?php echo $row['message_time']; ?></td>
-				<td><?php echo $row['message_sender']; ?></td>
-				<td><?php echo $row['message_subject']; ?></td>
-				<td><?php echo '<a href="answer.php?receiver="' . $row['message_sender_id'] . '> Répondre </a>'; ?></td>
-				<td><?php echo '<a href="deleteMessage.php?id="' . $row['message_id'] . '> Supprimer </a>'; ?></td>
-				<td><?php echo $row['']; ?></td>
-				
-			</tr>
-			<tr>
-				<button data-toggle="collapse" data-target="#message<?php echo $row['message_id'] ?>">Collapsible</button>
-
-				<div id="#message<?php echo $row['message_id'] ?>" class="collapse">
-					<td> <?php echo $row['message_message'] ?> </td>
-				</div>
-			</tr>
-			<?php }
-			
-			
+					<?php echo "<td>" . $row['message_subject'] . "</td>"; ?>
+					<?php echo "<td>" . $row['message_subject'] . "</td>"; ?>
+					<?php echo "<a href=\"answer.php?receiver=\"" . $row['message_sender_id'] . "\"> Répondre </a></td>"; ?>
+					<?php echo "<a href=\"deleteMessage.php?id=\"" . $row['message_id'] . "\"> Supprimer </a></td>"; ?>
+					
+					
+				</tr>
+				<tr>
+					<?php echo "<button data-toggle=\"collapse\" data-target=\"#message" . $row['message_id'] . "\"> Message </button>";?>
+					<?php echo "<div id=\"#message" . $row['message_id'] . "\" class=\"collapse\">" ;?>
+						<?php echo "<td>" . $row['message_message'] . "</td>";  ?>
+					<?php echo "</div>" ?>
+				</tr>
+			<?php 
+				};
 			?>
 		</table>
 		
