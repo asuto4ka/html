e2373f5e121f60c75f6f7593a8e9ebb10bfb7320
Thibault Schowing <thibault.schowing@heig-vd.ch>
2016-10-04 18:34:59 +0200

    test navbar home + css bootstrap

--- home.php
+++ home.php
@@ -6,34 +6,67 @@
  
 <html>
 
-   <head>
+	<head>
 
-      <meta charset="utf-8" />      
+		<meta charset="utf-8" />      
 
-      <link href="./css/style.php" rel="stylesheet" media="all" type="text/css">
+		<link href="./css/style.php" rel="stylesheet" media="all" type="text/css">
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 
       <title>STI Messenger</title>
       
-   </head>
+	</head>
 
-   <body>  
+	<body>  
 
-      <h1>STI Messenger</h1>
+		<h1>STI Messenger</h1>
 
-      <p> 
-         <?php
+		<p> 
+			<?php
             // Check if user session exist
             if (isset($_SESSION['userId'])) {
-               echo "Welcome ";
-               echo $_SESSION['userName'];
-               echo " !</br>";
-            }
-            else {
-               echo "Sorry, you must log in to access this page.";
-            }
-         ?>
-      </p>
+				   echo "Welcome ";
+				   echo $_SESSION['userName'];
+				   echo " !</br>";
+				}
+				else {
+				   echo "Sorry, you must log in to access this page.";
+				}
+			?>
+		</p>
+		
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+			  <a class="navbar-brand" href="messages.php">
+				Messages reçus
+			  </a>
+			</div>
+			<div class="navbar-header">
+			  <a class="navbar-brand" href="newMessage.php">
+				Nouveaux messages
+			  </a>
+			</div>
+			<div class="navbar-header">
+			  <a class="navbar-brand" href="#">
+				Changer le mot de passe
+			  </a>
+			</div>
+			<?php
+			if ($_SESSION['user_role'] == 1){
+				echo '<div class="navbar-header">
+			  <a class="navbar-brand" href="admin.php">
+				Administration
+			  </a>
+			</div>';
+			}
+			
+			
+			?>
+		  </div>
+		</nav>
 
-   </body>
+	</body>
 
 </html>
