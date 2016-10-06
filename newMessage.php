4e32a6a822634deb4ce356dfd4109af734dfb41e
root <root@localhost.localdomain>
2016-10-06 09:28:25 +0200

    backup txt after wrong user

--- newMessage.php
+++ newMessage.php
@@ -1,10 +1,20 @@
 <?php
    	session_start();  
 	include("databaseConnection.php");
+
+
    //On récupère qui seront les éxpéditeurs et les déstinataires
 	
 	
-	
+	//TODO problèmes mémorisation message quand destinataire faux
+	// Requêtes sql avec prepared statment ci-dessous
+/*
+$smt = $db->prepare("insert into names (name, email) values (':name', ':email')");
+$smt->bindValue(':name', $_POST['post_name'], SQLITE3_TEXT);
+$smt->bindValue(':email', $_POST['post_email'], SQLITE3_TEXT);
+
+$smt->execute();
+*/
 	
 		
 	//$to = $_GET["message_receiver_id"];
@@ -21,10 +31,12 @@
       
       <?php
 		include("includes/header.php");
+		
+		
 
 		function getUserName($id){
 			global $file_db;
-			//TODO secure $id 
+			//TODO use prepared statment 
 			$sql = "SELECT user_name FROM users WHERE user_id = " . $id;
 			//echo "<br/>[debug] sql: ". $sql;
 			$name =  $file_db->query($sql);
@@ -37,7 +49,7 @@
 		
 		function getUserId($name){
 			global $file_db;
-			//TODO secure $name
+			//TODO use prepared statment
 			$sql = "SELECT user_id FROM users WHERE user_name = '" . $name . "'";
 			//echo "<br/>[debug] sql: ". $sql;
 			$id =  $file_db->query($sql);
@@ -63,7 +75,7 @@
 			echo "<br/>[debug] Saving message in database";
 			
 			global $file_db;
-			//TODO secure all input
+			//TODO use prepared statment
 			$sql = "INSERT INTO messages (message_subject, message_message, message_sender_id , message_receiver_id)
 				VALUES ('" . $subject . "','". $message ."',". $_SESSION['userId'] .",". $userIdTo.")";
 				 
@@ -80,30 +92,33 @@
    <body>  
 	
 	<?php
-		include("includes/menu.php");
-	
-	
-	
-	// Si on a l'id depuis GET
-	/*if(isset($_GET['message_receiver_id'])){
-		echo "<br/>[debug] User id from GET: " . $_GET['message_receiver_id'];
-		$to = getUserName(htmlspecialchars(addslashes($_GET['message_receiver_id'])));
-		echo "<br/>[debug] Get user name from id: " . $to;
-		//remplis le formulaire avec $to -> nom d'utilisateur destinataire;
-	}*/
+	include("includes/menu.php");
 	
 	// SI on a le NOM (username)  depuis POST
+
 	if(isset($_POST['form_to'])){
-		echo "<br/>[debug] Username from POST: " . $_POST['form_to'];
+		//echo "<br/>[debug] Username from POST: " . $_POST['form_to'];
 		$id = getUserId($_POST['form_to']);
-		echo "<br/>[debug] User id with function: " . $id;
+		//echo "<br/>[debug] User id with function: " . $id;
 		//Vérifier si existe
+		
 		if($id == false){
 			// FALSE USER NAME - bandeau d'erreur
 			echo "<div class=\"container\"><div class=\"alert alert-danger\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Unknown user !</strong> Please enter an existing user !</div></div>";
+			// backup message et sujet si username faux, permet de ne pas retaper le message
+			
+			$subjectbkp = "";
+			$messagebkp = "";
+			if(isset($_POST['form_message']) && !empty($_POST['form_message'])){
+				$messagebkp = $_POST['form_message'];
+			}
+			
+			if(isset($_POST['form_subject']) && !empty($_POST['form_subject'])){
+				$subjectbkp = $_POST['form_subject'];
+			}
 		}
 		else{
-			echo "<br/>[debug] User id : " . $id;
+			//echo "<br/>[debug] User id : " . $id;
 		
 			//Si on a un message et sujet
 			if (isset($_POST['form_message']) && !empty($_POST['form_message']) && isset($_POST['form_subject']) && !empty($_POST['form_subject'])){
@@ -136,15 +151,7 @@
 	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"> 
 		
 		<?php 
-			// backup message et sujet si username faux, permet de ne pas retaper le message
-			$messagebkp = "";
-			if(isset($_POST['form_message']) && !empty($_POST['form_message'])){
-				$messagebkp = $_POST['form_message'];
-			}
-			$subjectbkp = "";
-			if(isset($_POST['form_subject']) && !empty($_POST['form_subject'])){
-				$subjectbkp = $_POST['form_subject'];
-			}
+			
 		?>
 
          	<div class="form-group">
@@ -155,7 +162,7 @@
     			<label for="email">Subject:</label> 
 			<input type="text" name="form_subject" id="form_subject" placeholder="Subject" value="<?php echo $subjectbkp; ?>"/>
 		</div>
-            		<textarea class="form-control" rows="5" type="text" name="form_message" id="form_message" placeholder="Type your message here" value="<?php echo $messagebkp; ?>"></textarea>
+            		<textarea class="form-control" rows="5" type="text" name="form_message" id="form_message" placeholder="Type your message here" ><?php echo $messagebkp; ?></textarea>
             	<br>
          	</div>
          
